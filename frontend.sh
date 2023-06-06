ba91f7a8b16ab7f307b19f8e31b6087d8960c5d1
Mango-DevopsEngineer <nakkabhavani1996@gmail.com>
2023-06-06 17:47:39 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,16 @@
 echo -e "\e[31mHInstalling the bginx server\e[0m"
-yum install nginx -y 
+yum install nginx -y
 
-echo -e "\e[32mRemovie the old content\e[0m"
+echo -e "\e[32mRemove the old content\e[0m"
 rm -rf /usr/share/nginx/html/* 
 
 echo -e "\e[33mdownloading the frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 
-echo -e "\e[31muextracting the frontend content\e[0m"
+echo -e "\e[31mextracting the frontend content\e[0m"
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 
 echo -e "\e[31mstarting nginx server\e[0m"
 systemctl enable nginx 
-systemctl restart nginx 
+systemctl restart nginx
