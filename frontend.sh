902f7cda4ca3515744ead902b7431525ad0f58a9
Mango-DevopsEngineer <nakkabhavani1996@gmail.com>
2023-06-06 23:29:29 +0530

    mongodb

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,16 @@
-echo -e "\e[31mInstalling the bginx server\e[0m"
-yum install nginx -y >/tmp/roboshop.log
+echo -e "\e[31mInstalling the nginx server\e[0m"
+yum install nginx -y &>>/tmp/roboshop.log
 
 echo -e "\e[32mRemove the old content\e[0m"
-rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
 echo -e "\e[33mdownloading the frontend content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log 2>/tmp/error.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log 2&>>/tmp/error.log
 
 echo -e "\e[34mextracting the frontend content\e[0m"
-cd /usr/share/nginx/html >/tmp/roboshop.log
-unzip /tmp/frontend.zip >/tmp/roboshop.log
+cd /usr/share/nginx/html &>>/tmp/roboshop.log
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
 echo -e "\e[35mstarting nginx server\e[0m"
-systemctl enable nginx >/tmp/roboshop.log
+systemctl enable nginx &>>/tmp/roboshop.log
 systemctl restart nginx 
