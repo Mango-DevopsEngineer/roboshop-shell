afd89042ce93289ae00038785f7459150a96cd0b
Mango-DevopsEngineer <nakkabhavani1996@gmail.com>
2023-06-06 18:15:40 +0530

    redirects

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,15 @@
 echo -e "\e[31mInstalling the bginx server\e[0m"
-yum install nginx -y
+yum install nginx -y >/tmp/roboshop.log
 
 echo -e "\e[32mRemove the old content\e[0m"
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
 
 echo -e "\e[33mdownloading the frontend content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log
 
 echo -e "\e[31mextracting the frontend content\e[0m"
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html >/tmp/roboshop.log
+unzip /tmp/frontend.zip >/tmp/roboshop.log
 
 echo -e "\e[31mstarting nginx server\e[0m"
 systemctl enable nginx 
