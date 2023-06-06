db219891ff76b239452dab821c72378e8bb3821a
Mango-DevopsEngineer <nakkabhavani1996@gmail.com>
2023-06-06 18:53:46 +0530

    redirect

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,7 @@ echo -e "\e[32mRemove the old content\e[0m"
 rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
 
 echo -e "\e[33mdownloading the frontend content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log 2>/tmp/error.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log 
 
 echo -e "\e[34mextracting the frontend content\e[0m"
 cd /usr/share/nginx/html >/tmp/roboshop.log
