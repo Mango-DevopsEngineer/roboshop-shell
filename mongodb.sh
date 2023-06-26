26187b890a7c8cd051b82ff65f007ef6c9e1d78d
Mango-DevopsEngineer <nakkabhavani1996@gmail.com>
2023-06-26 22:56:46 +0530

    mangodb corrected

--- mongodb.sh
+++ mongodb.sh
@@ -5,7 +5,7 @@ echo -e "\e[32mInstalling the mongodb server\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mupdate listen address\e[0m"
-sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[34mstarting nginx server\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
