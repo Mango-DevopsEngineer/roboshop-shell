70c8ec72e08897d8338bdff621d7a630064deb20
Mango-DevopsEngineer <nakkabhavani1996@gmail.com>
2023-06-27 11:30:10 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,9 +1,40 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
-cd /app 
-unzip /tmp/catalogue.zip
-cd /app 
-npm install 
+echo -e "\e[34mconfiguring the nodejs repos\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+
+echo -e "\e[34mInstalling nodejs\e[0m"
+yum install nodejs -y &>>/tmp/roboshop.log
+
+echo -e "\e[34mAdding Application user\e[0m"
+useradd roboshop &>>/tmp/roboshop.log
+
+echo -e "\e[34mcreate Application directory\e[0m"
+rm -rf /app &>>/tmp/roboshop.log
+mkdir /app &>>/tmp/roboshop.log
+
+echo -e "\e[34mdownloading the Application content\e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
+
+echo -e "\e[34mExtract Application content\e[0m"
+unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
+
+echo -e "\e[34mInstall nodejs depandencies\e[0m"
+npm install &>>/tmp/roboshop.log
+
+echo -e "\e[34msetup systemD service\e[0m"
+cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+
+echo -e "\e[34mstart catalogue service \e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable catalogue &>>/tmp/roboshop.log
+systemctl start catalogue &>>/tmp/roboshop.log
+
+echo -e "\e[34mcopy mongodb repo file\e[0m"
+cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+
+echo -e "\e[34mInstalling the mongodb client\e[0m"
+yum install mongodb-org-shell -y &>>/tmp/roboshop.log
+
+echo -e "\e[34mload schema\e[0m" 
+mongo --host mongodb-dev.mangoengineer.store </app/schema/catalogue.js &>>/tmp/roboshop.log
